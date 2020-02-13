55419481bcf5638dd45ca139954441e131794df6
Hoshino0116 <60649916+Hoshino0116@users.noreply.github.com>
2020-02-13 17:41:59 +0900

    Update 100rows-deeplearning.py

--- 100rows-deeplearning.py
+++ 100rows-deeplearning.py
@@ -2,10 +2,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 from torch import optim
-import torch.nn.functional as F
-from torch.autograd import Variable
 import torch.nn.init as init
-
 import torchvision.transforms as transforms
 from torchvision import models
 from torch.utils.data import Dataset,DataLoader
@@ -30,28 +27,31 @@ criterion = nn.CrossEntropyLoss()
 
 optimizer = optim.SGD(net.parameters(),lr=0.01,momentum=0.9,weight_decay=0.00005)
 
-loss = 0
-epoch_loss = 0
-count = 0
+loss, epoch_loss, count = 0, 0, 0
 acc_list = []
 loss_list = []
 
 #訓練・推論
-for i in range(1000):
-  
+for i in range(50):
+
+  #ここから学習
   net.train()
-  
+
   for j,data in enumerate(train_loader,0):
     optimizer.zero_grad()
-    inputs,labels = data
 
+    #1:訓練データを読み込む
+    inputs,labels = data
     inputs = inputs.to(device)
     labels = labels.to(device)
 
+    #2:計算する
     outputs = net(inputs)
 
+    #3:誤差を求める
     loss = criterion(outputs,labels)
 
+    #4:誤差から学習する
     loss.backward()
     optimizer.step()
 
@@ -62,24 +62,27 @@ for i in range(1000):
   print('%depoch:mean_loss=%.3f\n'%(i+1,epoch_loss/count))
   loss_list.append(epoch_loss/count)
 
-  epoch_loss = 0
-  count = 0
-  correct = 0
-  total = 0
+  epoch_loss, count = 0, 0
+  correct,total = 0, 0
   accuracy = 0.0
+
+  #ここから推論
   net.eval()
- 
+
   for j,data in enumerate(test_loader,0):
 
+    #テストデータを用意する
     inputs,labels = data
-
     inputs = inputs.to(device)
     labels = labels.to(device)
 
+    #計算する
     outputs = net(inputs)
 
+    #予測値を求める
     _,predicted = torch.max(outputs.data,1)
 
+    #精度を計算する
     correct += (predicted == labels).sum()
     total += batch_size
 
